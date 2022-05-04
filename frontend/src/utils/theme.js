7a9db2b44414850a4e900a14def820423c67e8d3
First-Derivative <ashraff.hatz@gmail.com>
2022-05-05 02:15:58 +0800

    Merge development-contrast for experimental accent color

--- frontend/src/utils/theme.js
+++ frontend/src/utils/theme.js
@@ -7,6 +7,10 @@ export const lightTheme = {
 
 export const darkTheme = {
   body: '#0a0a0a',
+<<<<<<< HEAD
   text: '#7A6E6E',
+=======
+  text: 'white',
+>>>>>>> development-contrast
   toggleBorder: '#6B8096',
 }
