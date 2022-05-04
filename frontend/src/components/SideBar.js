3faa48b4f765beb9926b8af1051b9ff407f8c06c
First-Derivative <ashraff.hatz@gmail.com>
2022-05-04 17:47:25 +0800

    Add text-outline feature for mobile

--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -16,7 +16,7 @@ function SideBar() {
       <div className="col-12 col-sm-3" id='sidebar-container'>
         <div className='d-flex flex-column justify-content-between align-items-center' id="sidebar-content">
           <DarkmodeSwitch />
-          <div className='text-center my-0 mb-5 my-sm-auto mx-0 mx-sm-5' style={darkStyle}>
+          <div className='text-center my-0 mb-5 my-sm-auto mx-0 mx-sm-5' style={darkStyle} id="sidebar-main-content">
             <div className="h1 mt-0 mt-sm-5">
               Ashraff Hatz
             </div>
