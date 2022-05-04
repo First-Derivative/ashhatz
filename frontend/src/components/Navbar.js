9277add92e178ef7e576a9eaa3f573c77ed49bd3
First-Derivative <ashraff.hatz@gmail.com>
2022-05-04 23:03:01 +0800

    Add UI animations for Navbar contents & darkmode support

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,12 +1,23 @@
 import React from 'react'
+import './Navbar.css'
+import { darkTheme, lightTheme} from '../utils/theme' 
+import { useDarkmode } from '../DarkmodeContext'
 
 function Navbar() {
+
+  const darkmode = useDarkmode();
+
+  const active = {
+    color: `${darkmode ? darkTheme.body : lightTheme.body}`,
+    backgroundColor: `${darkmode ? darkTheme.text : lightTheme.text}`
+  }
+
   return (
     <div className="row gy-4 gy-sm-5 text-center justify-content-evenly">
       
-      <div className="col-6 col-sm-3 px-3">
+      <div className="col-6 col-sm-3 px-3 ">
         
-        <div className="h5 my-auto py-4 border border-light">
+        <div style={active}  className={`h5 my-auto mx-auto py-3 w-75 btn-nav target ${ darkmode ? "dark-btn-nav" : "" }`}>
           Stats
         </div>
       
@@ -14,7 +25,7 @@ function Navbar() {
 
       <div className="col-6 col-sm-3 px-3">
         
-        <div className="h5 my-auto py-4 border border-light">
+        <div className={`h5 my-auto mx-auto py-3 w-75 btn-nav target ${ darkmode ? 'dark-btn-nav' : '' }`}>
           Profile
         </div>
       
@@ -22,7 +33,7 @@ function Navbar() {
 
       <div className="col-6 col-sm-3 px-3">
         
-        <div className="h5 my-auto py-4 border border-light">
+        <div className={`h5 my-auto mx-auto py-3 w-75 btn-nav target ${ darkmode ? 'dark-btn-nav' : '' }`}>
           Literature
         </div>
       
@@ -30,7 +41,7 @@ function Navbar() {
 
       <div className="col-6 col-sm-3 px-3">
         
-        <div className="h5 my-auto py-4 border border-light">
+        <div className={`h5 my-auto mx-auto py-3 w-75 btn-nav target ${ darkmode ? 'dark-btn-nav' : '' }`}>
           Movies
         </div>
       
