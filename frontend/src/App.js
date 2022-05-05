4173f097dd98d045ef65b6a8c2a240c3da6566c9
First-Derivative <ashraff.hatz@gmail.com>
2022-05-06 03:31:26 +0800

    Add Literature page and routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 
 import Homepage from './pages/Homepage';
 import Profile from './pages/Profile';
+// import Media from './pages/Media';
+import Literature from './pages/Literature';
 import { DarkmodeChannel } from './DarkmodeContext';
 import { Routes, Route, Navigate} from 'react-router-dom';
 import GlobalStyle from './utils/GlobalStyles';
@@ -30,6 +32,8 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Homepage/>} />
         <Route exact path="/profile" element={<Profile/>} />
+        <Route exact path="/literature" element={<Literature/>} />
+        {/* <Route exact path="/media" element={<Media/>} /> */}
         <Route
         path="*"
         element={<Navigate to="/" replace />}
