3477c5467c78752e4af2546f328088d992222e76
First-Derivative <ashraff.hatz@gmail.com>
2022-05-16 18:07:56 +0800

    Add frontend axiosInstance utility

---
+++ frontend/src/utils/axios.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+const baseURL = process.env.BACKEND_URL;
+
+axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
+axios.defaults.xsrfCookieName = "csrftoken";
+
+const axiosInstance = axios.create({
+  baseURL: baseURL,
+  timeout: 10000, // 10 seconds for heroku spinup
+  headers: {
+    'Content-Type': 'application/json',
+    accept: 'application/json'
+  },
+})
+
+export default axiosInstance;
