fc466f37b95fbc61524dc904a89ef11d68e61d39
First-Derivative <ashraff.hatz@gmail.com>
2022-05-17 01:47:55 +0800

    Add Login Logout View & routing

--- users/views.py
+++ users/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.contrib import auth
 from django.utils.decorators import method_decorator
-from rest_framework import status
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from django.views.decorators.csrf import ensure_csrf_cookie
+from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect
 from .models import User
 from .serializers import UserSerializer
 from utils.ErrorResponse import ErrorResponse
@@ -15,7 +14,7 @@ def handler_not_found(request, exception):
 
 @method_decorator(ensure_csrf_cookie, name='dispatch')
 class GetCSRFToken(APIView):
-  permission_classes = [IsAuthenticated]
+  permission_classes = [AllowAny]
 
   def get(self, request, format=None):
     return Response({'success': 'CSRF Token set'})
@@ -30,7 +29,6 @@ class CheckAuth(APIView):
       if(user.is_authenticated):
         serialized = UserSerializer(user)
         return Response(serialized.data, status=200)
-        print(user, 'is not authenticated')
       else:
         error = ErrorResponse('User not Authenticated', 401)
         return Response(error.message, status=error.status)
@@ -38,3 +36,36 @@ class CheckAuth(APIView):
       print(err)
       error = ErrorResponse('Cannot find User', 401)
       return Response(error.message, status=error.status)
+
+@method_decorator(csrf_protect, name='dispatch')
+class LoginView(APIView):
+  permission_classes = [AllowAny]
+
+  def post(self, request, format=None):
+    data = self.request.data
+    email = data['email']
+    password = data['password']
+
+    try:
+      user = auth.authenticate(email=email, password=password)
+
+      if user is not None:
+        auth.login(request, user)
+        serialized = UserSerializer(user)
+        return Response(serialized.data, status=200)
+      else:
+        error = ErrorResponse('Credentials Incorrect', 401)
+        return Response(error.message,  status=error.status)
+    except:
+      error = ErrorResponse('Server authentication error', 400)
+      return Response(error.message, status=error.status)
+
+class LogoutView(APIView):
+  permission_classes = [IsAuthenticated]
+
+  def post(self, request, format=None):
+    try:
+      auth.logout(request)
+      return Response({ 'success': 'Loggout Out' })
+    except:
+      return Response({ 'error': 'Something went wrong when logging out' })
