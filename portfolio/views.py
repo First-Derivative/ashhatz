c7c947c673231cc6733a3543ba2c66a9ecb9bc34
First-Derivative <ashraff.hatz@gmail.com>
2022-07-29 14:56:10 +0800

    Update portfolio GetProjects API permissions

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from .models import * 
 from .serializers import *
@@ -10,6 +10,7 @@ from utils.ErrorResponse import ErrorResponse
 class GetProject(APIView):
 
   def get(self, request, id, format=None):
+  
     try:
       project = Project.objects.get(id=id)
       serialized = ProjectSerializer(project, many=True)
@@ -19,6 +20,6 @@ class GetProject(APIView):
       return Response(error.message, status=error.status)
 
 class GetProjects(ListAPIView):
-  permission_classes = [IsAuthenticated]
+  permission_classes = [AllowAny]
   queryset = Project.objects.all()
   serializer_class = ProjectSerializer
