6d432ca6a6333d1794667782b9abc5ee218afabb
First-Derivative <ashraff.hatz@gmail.com>
2022-07-30 20:52:47 +0800

    Update createTest view to accommodate POST data

--- math4kids/views.py
+++ math4kids/views.py
@@ -1,12 +1,30 @@
 from .source import Test, Question
+from utils.ErrorResponse import ErrorResponse
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
-@api_view(['GET'])
+# Auxiliary Function that checks data from POST request
+def validate_data(data):
+
+  fields = ["range", "no_mult", "no_div", "bound_upper", "bound_lower"]
+
+  for field in data.keys():
+    if(field not in fields):
+      return False
+  return True
+
+@api_view(['POST'])
 @permission_classes([AllowAny])
 def createTest(request):
-  new_test = Test(5, 5, 1, 12)
+
+  data = request.data
+
+  # Validate POST Data
+  if(not validate_data(data)):
+    return ErrorResponse(message="Form error, please refresh the page")
+
+  new_test = Test(data["no_mult"], data["no_div"], data["bound_lower"], data["bound_upper"])
   data = new_test.jsonify()
   return Response({"test": data})
 
