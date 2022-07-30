fd6f339f423cf1e4094fe8d0a9de4c822d1c5e21
First-Derivative <ashraff.hatz@gmail.com>
2022-07-30 21:33:38 +0800

    Fix ErrorResponse bug in createTest view

--- math4kids/views.py
+++ math4kids/views.py
@@ -9,8 +9,8 @@ def validate_data(data):
 
   fields = ["range", "no_mult", "no_div", "bound_upper", "bound_lower"]
 
-  for field in data.keys():
-    if(field not in fields):
+  for field in fields:
+    if(field not in data.keys()):
       return False
   return True
 
@@ -22,7 +22,8 @@ def createTest(request):
 
   # Validate POST Data
   if(not validate_data(data)):
-    return ErrorResponse(message="Form error, please refresh the page")
+    error = ErrorResponse(message="Form error, please refresh the page")
+    return Response(error.message, status=error.status)
 
   new_test = Test(data["no_mult"], data["no_div"], data["bound_lower"], data["bound_upper"])
   data = new_test.jsonify()
