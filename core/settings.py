9a4a8ea97ea4fba50df71b794579e91429caf559
First-Derivative <ashraff.hatz@gmail.com>
2022-08-28 19:33:52 +0100

    Convert from db.sqlite3 -> postgres heroku aws db

--- core/settings.py
+++ core/settings.py
@@ -92,8 +92,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("db_name"),
+        'USER': os.environ.get("db_user"),
+        'PASSWORD': os.environ.get("db_pass"),
+        'HOST': os.environ.get("db_host"),
+        'PORT': os.environ.get("db_port"),
+
     }
 }
 
