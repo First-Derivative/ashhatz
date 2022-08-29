3796133d7ce6b81dd49a601b8c98f80bf6893eea
First-Derivative <ashraff.hatz@gmail.com>
2022-08-29 15:54:04 +0100

    Implement ProjectCard component

---
+++ frontend/src/components/ProjectCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ProjectCard({project}) {
+  return (
+    <div className="card" style={{width: '16rem'}}>
+      <img src="https://images.pexels.com/photos/4693135/pexels-photo-4693135.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="card-img-top" alt="ProjectCardImage"/>
+        <div className="card-body">
+          <p className="card-text">{project.name}</p>
+        </div>
+    </div>
+  )
+}
+
+export default ProjectCard
