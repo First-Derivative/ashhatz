e812c31b014d4e17955b24b56c7e3035933ac0ca
First-Derivative <ashraff.hatz@gmail.com>
2022-08-29 15:53:55 +0100

    Implement PortfolioContent render of ProjectCard child components

--- frontend/src/components/PortfolioContent.js
+++ frontend/src/components/PortfolioContent.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import ProjectCard from './ProjectCard'
 
-function PortfolioContent() {
+function PortfolioContent({projects}) {
+
   return (
-    <div>
-      PortfolioContent
+    <>
+    <div className="h5 mx-auto ms-4">
+      Commercial Applications
     </div>
+
+    <div class="mx-auto ms-4 mt-3">
+      {projects.map((project, index) => (
+        <ProjectCard 
+        key={index}
+        project={project}
+        />
+      ))}
+    </div>
+    </>
   )
 }
 
