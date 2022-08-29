9a80d22938dbcb38feeeebba5256b0985a5e3f1a
First-Derivative <ashraff.hatz@gmail.com>
2022-08-29 15:21:55 +0100

    Implement Homepage AJAX REST call to backend

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -2,13 +2,37 @@ import React from 'react'
 import SideBar from '../components/SideBar'
 import AppContent from '../components/AppContent'
 import PortfolioContent from '../components/PortfolioContent'
+import axiosInstance from '../utils/axios'
+import { useState, useEffect } from 'react'
+import ErrorAlert from './ErrorAlert'
 
 function Homepage() {
+
+  const [projects, setProjects] = useState([])
+
+  const getProjects = async () => {
+    
+    await axiosInstance.get(`portfolio/api/get/projects/`).then(
+      (res) => {
+        setProjects(res.data)
+      }
+    ).catch(
+      (err) => {
+        console.log("got error")
+        console.log(err)
+      }
+    )
+  }
+
+  useEffect( () => {
+    getProjects()
+  }, [])
 
   return (
       <div className="row">
         <SideBar />
         <AppContent title={'Portfolio'}>
+
           <PortfolioContent
           />
         </AppContent>
