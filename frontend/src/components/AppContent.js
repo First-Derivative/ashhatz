183eeaf50634852da9237a2737275adab66e75ec
First-Derivative <ashraff.hatz@gmail.com>
2022-08-29 15:51:05 +0100

    Update AppContent styling for content

--- frontend/src/components/AppContent.js
+++ frontend/src/components/AppContent.js
@@ -5,14 +5,17 @@ function AppContent({title, children}) {
   return (
     <div className="col-12 col-sm-9 p-4 p-sm-5">
       <Navbar />
+
       <div className="row mt-5 ">
         <div className="col-12 text-start px-3">
             <div className="h2 mx-auto ms-4"> {title} </div>
         </div>
       </div>
 
-      <div className="mt-5" id="AppContent">
-        {children}
+      <div className="row mt-3" id="AppContent">
+        <div className="col-12 text-start px-3">
+          {children}
+        </div>
       </div>
 
     </div>
