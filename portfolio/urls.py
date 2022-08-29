2067166a321e05938914dfb2da02ef466bf5770f
First-Derivative <ashraff.hatz@gmail.com>
2022-08-29 01:45:00 +0100

    Fix GET all ProjectLinks url bug

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
   path('api/get/project/<slug:id>', GetProject.as_view(), name="api-get-project"),
   path('api/get/tags/', GetProjectTags.as_view(), name="api-get-all-projecttags"),
   path('api/get/tag/<slug:id>', GetProjectTag.as_view(), name="api-get-projecttag"),
-  path('api/get/link/', GetProjectLinks.as_view(), name="api-get-all-projectlinks"),
-  path('api/get/tag/<slug:id>', GetProjectLink.as_view(), name="api-get-projectlink"),
+  path('api/get/links/', GetProjectLinks.as_view(), name="api-get-all-projectlinks"),
+  path('api/get/link/<slug:id>', GetProjectLink.as_view(), name="api-get-projectlink"),
 ]
