d5676f8dc1a24d2816ff2c05ea2235c6b757cf7d
First-Derivative <ashraff.hatz@gmail.com>
2022-08-30 14:19:41 +0100

    Update django settings with email configuration

--- core/settings.py
+++ core/settings.py
@@ -178,3 +178,11 @@ CORS_ALLOWED_ORIGINS = [
 SECURE_SSL_REDIRECT = ssl_mode
 SESSION_COOKIE_SECURE = ssl_mode
 CSRF_COOKIE_SECURE = ssl_mode
+
+# Email configuration 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get("email-user")
+EMAIL_HOST_PASSWORD = os.environ.get("email-pass")
