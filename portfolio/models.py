07e81465e9599a83877722ec91e8a255fb4ba49f
First-Derivative <ashraff.hatz@gmail.com>
2022-09-04 00:21:47 +0100

    Implement Portfolio Model ProjectMedia

--- portfolio/models.py
+++ portfolio/models.py
@@ -9,11 +9,11 @@ class Project(models.Model):
   id = models.BigAutoField(primary_key=True)
   name = models.CharField(max_length=50, default="New Project")
   summary = models.CharField(max_length=500, null=True, default="summary")
-  media = models.CharField(max_length=100, null=True, blank=True)
 
   # Relationships
   tags = models.ManyToManyField('ProjectTag')
   links = models.ManyToManyField('ProjectLink')
+  media = models.ManyToManyField('ProjectMedia')
 
   # Instance metadata
   created = models.DateTimeField(auto_now_add=True)
@@ -41,16 +41,27 @@ class ProjectTag(models.Model):
     return self.name
 
 class ProjectLink(models.Model):
-  ordering = ('id')
   
   class Meta: 
     verbose_name = "Project Link"
     verbose_name_plural = "Project Links"
+    ordering = ('id',)
 
   id = models.BigAutoField(primary_key=True)
   name = models.CharField(max_length=50)
   url = models.CharField(max_length=50)
   media = models.CharField(max_length=100, blank=True)
+
+  def __str__(self):
+    return "{}:{}".format(self.name, self.url)
+
+
+class ProjectMedia(models.Model):
+  class Meta:
+    ordering = ('id',)
+
+  name = models.CharField(max_length=50, blank=True, null=True)
+  url = models.CharField(max_length=200, blank=False, null=False)
 
   def __str__(self):
     return "{}:{}".format(self.name, self.url)
